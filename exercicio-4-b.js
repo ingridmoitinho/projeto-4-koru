b56722affebb46148f2eed78be37aa917260a644
GiovanaMarques-dev <giovana.smarques0@gmail.com>
2025-07-27 16:17:36 -0300

    Resposta do exc. 4-b

---
+++ exercicio-4-b.js
@@ -0,0 +1,17 @@
+// Desafio: b. Use some para verificar se existe alguma tarefa de baixa prioridade completa
+
+const tasks = [
+  { id: 1, title: "Concluir projeto", completed: false, priority: "alta" },
+  { id: 2, title: "Ler emails", completed: true, priority: "média" },
+  { id: 3, title: "Participar da reunião", completed: false, priority: "alta" },
+  { id: 4, title: "Comprar mantimentos", completed: true, priority: "baixa" },
+  { id: 5, title: "Estudar JavaScript", completed: false, priority: "média" },
+];
+
+const completedLowPriority = tasks.some(
+  (task) => task.priority === "baixa" && task.completed
+);
+console.log(
+  "Existe tarefa de baixa prioridade completa?",
+  completedLowPriority
+);
